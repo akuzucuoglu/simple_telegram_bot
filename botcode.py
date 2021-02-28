0a599e89dff3c1b246520002fb21f6ddcbb3b39e
Ahmet Kuzucuoglu <ahmetkuzucuoglu@gmail.com>
2021-02-28 20:43:36 +0300

    update botcode.py avoid channel posts

--- botcode.py
+++ botcode.py
@@ -58,28 +58,29 @@ def main():
 
         if len(all_updates) > 0:
             for current_update in all_updates:
-                print(current_update)
-                first_update_id = current_update['update_id']
-                if 'text' not in current_update['message']:
-                    first_chat_text='New member'
-                else:
-                    first_chat_text = current_update['message']['text']
-                first_chat_id = current_update['message']['chat']['id']
-                if 'first_name' in current_update['message']:
-                    first_chat_name = current_update['message']['chat']['first_name']
-                elif 'new_chat_member' in current_update['message']:
-                    first_chat_name = current_update['message']['new_chat_member']['username']
-                elif 'from' in current_update['message']:
-                    first_chat_name = current_update['message']['from']['first_name']
-                else:
-                    first_chat_name = "unknown"
-
-                if first_chat_text == 'Hi':
-                    journos_bot.send_message(first_chat_id, 'Morning ' + first_chat_name)
-                    new_offset = first_update_id + 1
-                else:
-                    journos_bot.send_message(first_chat_id, 'How are you doing '+first_chat_name)
-                    new_offset = first_update_id + 1
+                if 'message' in current_update.keys:    
+                    print(current_update)
+                    first_update_id = current_update['update_id']
+                    if 'text' not in current_update['message']:
+                        first_chat_text='New member'
+                    else:
+                        first_chat_text = current_update['message']['text']
+                    first_chat_id = current_update['message']['chat']['id']
+                    if 'first_name' in current_update['message']:
+                        first_chat_name = current_update['message']['chat']['first_name']
+                    elif 'new_chat_member' in current_update['message']:
+                        first_chat_name = current_update['message']['new_chat_member']['username']
+                    elif 'from' in current_update['message']:
+                        first_chat_name = current_update['message']['from']['first_name']
+                    else:
+                        first_chat_name = "unknown"
+    
+                    if first_chat_text == 'Hi':
+                        journos_bot.send_message(first_chat_id, 'Morning ' + first_chat_name)
+                        new_offset = first_update_id + 1
+                    else:
+                        journos_bot.send_message(first_chat_id, 'How are you doing '+first_chat_name)
+                        new_offset = first_update_id + 1
 
 
 if __name__ == '__main__':
